afc80b3bf953b22482a7bac8a83154d322468db9
Rabia464 <u2024527@giki.edu.pk>
2025-02-23 15:45:29 +0500

    calling func pointer indirectly

--- funpointers.cpp
+++ funpointers.cpp
@@ -1,12 +1,27 @@
+// #include<iostream>
+// using namespace std;
+// // int add(int a, int b){
+// // return a+b;
+// //}
+
+// int main(){
+//     // //function pointer declaration: 
+//     // int (*funcpoint)(int,int)=&add;
+//     // int result = funcpoint(4,4);//calling function through pointer
+//     // cout<<"Result: "<<result;
+    
+//     return 0;
+// }
+                            //*********** Caling function pointer indirectly***********
 #include<iostream>
 using namespace std;
-int add(int a, int b){
-return a+b;
+float area(float b, float h){
+    return b*h;
 }
 int main(){
-    //function pointer declaration: 
-    int (*funcpoint)(int,int)=&add;
-    int result = funcpoint(4,4);
-    cout<<"Result: "<<result;
+    float(*funcpoint)(float,float);
+    funcpoint=area;
+    int result =funcpoint(5.67,2.4);
+    cout<<"Area of rectangle:  "<<result;
     return 0;
 }
